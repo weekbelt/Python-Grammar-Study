ba4aae2520dd8b26f4e2c7711811d169f0d1e18b
weekbelt <vfrvfr4207@gmail.com>
2020-09-09 19:32:15 +0900

    상속, 다중 상속

--- 클래스/class.py
+++ 클래스/class.py
@@ -1,17 +1,14 @@
+# 일반 유닛
 class Unit:
-    def __init__(self, name, hp, damage):
+    def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-        self.damage = damage
-        print("{0} 유닛이 생성 되었습니다.".format(self.name))
-        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
 
 
 # 공격 유닛
-class AttackUnit:
+class AttackUnit(Unit):
     def __init__(self, name, hp, damage):
-        self.name = name
-        self.hp = hp
+        Unit.__init__(self, name, hp)
         self.damage = damage
 
     def attack(self, location):
@@ -25,12 +22,23 @@ class AttackUnit:
             print("{0} : 파괴되었습니다.".format(self.name))
 
 
-# 파이어뱃 : 공격 유닛, 화염방사기
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
+# 드랍쉽 : 공중 유닛, 수송기, 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
 
-# 공격 2번 받는다고 가정
-firebat1.damaged(25)
-firebat1.damaged(25)
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. 속도 [{2}]".format(name, location, self.flying_speed))
 
 
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
