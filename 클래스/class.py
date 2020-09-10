fe559e127373326e8b42dbb001d3303bb4a0b857
weekbelt <vfrvfr4207@gmail.com>
2020-09-10 18:58:13 +0900

    pass

--- 클래스/class.py
+++ 클래스/class.py
@@ -48,11 +48,23 @@ class FlyableAttackUnit(AttackUnit, Flyable):
         self.fly(self.name, location)
 
 
-vulture = AttackUnit("벌쳐", 80, 10, 20)
+# 건물
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+        pass
 
-battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
 
-vulture.move("11시")
-# battlecruiser.fly(battlecruiser.name, "9시")
-battlecruiser.move("9시")
+# 서플라이 디폿 : 건물, 1개 건물 = 8 유닛.
+supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
 
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+
+def game_over():
+    pass
+
+
+game_start()
+game_over()
