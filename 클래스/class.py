716af3200c5ff1cc17985c16c20a564746c987d2
weekbelt <vfrvfr4207@gmail.com>
2020-09-10 19:32:39 +0900

    스타크래프트 후반전

--- 클래스/class.py
+++ 클래스/class.py
@@ -1,3 +1,6 @@
+from random import *
+
+
 # 일반 유닛
 class Unit:
     def __init__(self, name, hp, speed):
@@ -7,7 +10,6 @@ class Unit:
         print("{0} 유닛이 생성되었습니다.".format(name))
 
     def move(self, location):
-        print("[지상 유닛 이동]")
         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
 
     def damaged(self, damage):
@@ -25,7 +27,7 @@ class AttackUnit(Unit):
         self.damage = damage
 
     def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]\n".format(self.name, location, self.damage))
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(self.name, location, self.damage))
 
 
 # 마린
@@ -83,7 +85,6 @@ class FlyableAttackUnit(AttackUnit, Flyable):
         Flyable.__init__(self, flying_speed)
 
     def move(self, location):
-        print("[공중 유닛 이동]")
         self.fly(self.name, location)
 
 
@@ -100,3 +101,65 @@ class Wraith(FlyableAttackUnit):
         else:   # 클로킹 모드 해제 -> 모드 설정
             print("{0} : 클로킹 모드 설정합니다.".format(self.name))
             self.clocked = True
+
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+
+def game_over():
+    print("Player : gg")    # good game
+    print("[Player] 님이 게임에서 퇴장하셨습니다.")
+
+
+# 실제 게임 시작
+game_start()
+
+# 마린 3기 생성
+m1 = Marine()
+m2 = Marine()
+m3 = Marine()
+
+# 탱크 2기 생성
+t1 = Tank()
+t2 = Tank()
+
+# 레이스 1기 생성
+w1 = Wraith()
+
+# 유닛 일괄 관리
+attack_units = []
+attack_units.append(m1)
+attack_units.append(m2)
+attack_units.append(m3)
+attack_units.append(t1)
+attack_units.append(t2)
+attack_units.append(w1)
+
+# 전군 이동
+for unit in attack_units:
+    unit.move("1시")
+
+# 탱크 시즈모드 개발
+Tank.seize_developed = True
+print("[알림] 탱크 시즈 모드 개발이 완료 되었습니다.")
+
+# 공격 모드 준비 (마린 스팀팩, 탱크 : 시즈모드, 레이스 : 클로킹)
+for unit in attack_units:
+    if isinstance(unit, Marine):
+        unit.stimpack()
+    elif isinstance(unit, Tank):
+        unit.set_seize_mode()
+    elif isinstance(unit, Wraith):
+        unit.clocking()
+
+# 전군 공격
+for unit in attack_units:
+    unit.attack("1시")
+
+# 전군 피해
+for unit in attack_units:
+    unit.damaged(randint(5, 21))    # 공격은 랜덤으로 받음 (5 ~ 20)
+
+# 게임 종료
+game_over()
